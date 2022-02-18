4d933599eb11364a743dae1f8ccae9db6d868b22
Deren B <dbozerprogramming@gmail.com>
2022-02-18 15:06:46 -0500

    Enemy despawn scripts

--- Space-Shmup/Space-Shoot-Em-Up/Assets/__Scripts/BoundsCheck.cs
+++ Space-Shmup/Space-Shoot-Em-Up/Assets/__Scripts/BoundsCheck.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 // To type the next 4 lines, start by typing /// and then Tab.
 /// <summary>
 /// Keeps a GameObject on screen.
+/// /// Checks whether a GameObject is on screen and can force it to stay on screen.
 /// Note that this ONLY works for an orthographic Main Camera at [ 0, 0, 0 ].
 /// </summary>
 
@@ -12,10 +13,15 @@ public class BoundsCheck : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     void Awake()
     {
@@ -26,26 +32,43 @@ public class BoundsCheck : MonoBehaviour
     void LateUpdate()
     {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
         if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            isOnScreen = false;
+            offRight = true;
         }
 
         if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            isOnScreen = false;
+            offLeft = true;
         }
 
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            isOnScreen = false;
+            offUp = true;
         }
 
         if (pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            isOnScreen = false;
+            offDown = true;
         }
-        transform.position = pos;
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }
     }
 
     // Draw the bounds in the Scene pane using OnDrawGizmos()
