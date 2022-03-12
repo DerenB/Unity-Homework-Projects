b561636d87b6201e89fe98306060c9c755499867
Deren B <dbozerprogramming@gmail.com>
2022-03-12 17:58:59 -0500

    Truck Prototype

--- Create-With-Code/Trucking/Assets/Scripts/PlayerController.cs
+++ Create-With-Code/Trucking/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // transform.Translate(0, 0, 1);
+        // transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
